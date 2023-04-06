9b385e36ff63d16e4eddfe0862af3fd2e09db24b
spailll <bsailor@okstate.edu>
2023-04-06 09:24:49 -0400

    First Commit

---
+++ notes.cpp
@@ -0,0 +1,70 @@
+
+//Included Libraries
+#include <iostream> 
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <cstdlib>
+
+//Definitions
+
+//Function Declarations
+void printFile(std::string path, std::string fileName);
+
+int main(int argc, char** argv) {
+  
+  //handling args
+  const std::string userName(std::getenv("USER"));
+  const std::string absolute_path = "/home/" + userName + "/Documents/Notes";
+  bool noArgs = true;                       //Boolean for detecting a no arguments call
+  
+  for (int i = 1; i < argc; ++i) {          //Loop to iterate through arguments
+    std::string arg = argv[i];
+    noArgs = false;
+
+    if (arg == "-l" || arg == "--list") {   //List out the possible notes to view
+      try {
+        int pathLength = absolute_path.length();
+        for (const auto& entry : std::filesystem::directory_iterator(absolute_path)) {
+          std::string entryPath = entry.path();
+          std::string member = entryPath.substr(pathLength + 1, entryPath.length() - pathLength - 1);
+          if (member.at(0) != '.' && member != "README.md") {        //Check if the file is hidden with .
+            std::cout << member << std::endl;
+          }
+        }
+      } catch (const std::filesystem::filesystem_error& e) { //FIXME:
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+      }
+    }   else if (arg == "-f" || arg == "--file") {  //Notes file to be viewed
+      i++;
+      if (i >= argc) { //ERROR: No File Path Specified
+        std::cerr << "Error: " << "No File Path Specified with -f flag" << std::endl;
+        return 1;
+      }
+      printFile(absolute_path, argv[i]);
+      return 1;
+    }   else {
+      std::cerr << "Error: " << "Unknown flag" << std::endl;
+    }
+  }
+
+  if (noArgs) { // open the command line UI
+    
+  }
+  return 1;
+}
+
+void printFile(std::string path, std::string fileName) {
+  std::ifstream inputFile(path + "/" + fileName);
+  if (!(inputFile.is_open())) {
+    std::cerr << "Error: " << "Unable to open notes at " << fileName << std::endl;
+    return;
+  }
+  std::string line;
+  while (std::getline(inputFile, line)) {
+    std::cout << line << std::endl;
+  }
+  inputFile.close();
+  return;
+}
